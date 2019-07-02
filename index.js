96dd022f6c5049ded0644609bbeed2eaad31f556
E664991 <e664991@vanilla-ubuntu.austin.sttgts.com>
2019-07-02 14:33:30 -0500

    spaces and comments

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+// get and listen
 app.get('/', (req, res) => res.send('Hello world'));
 app.listen(PORT, () => console.log('example boi'));
