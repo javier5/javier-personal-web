cf6797d4fb3991d970b4df2a6d81f68b83c70fbf
E664991 <e664991@vanilla-ubuntu.austin.sttgts.com>
2019-07-02 14:22:55 -0500

    changed the index

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello world'));
+app.listen(port, () => console.log('example boi'));
